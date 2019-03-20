0941bf2559f94afee422f324e15f3bc6a4a06b07
singhjason96 <jason.singh@baruchmail.cuny.edu>
2019-03-20 13:09:29 -0400

    set up models and controllers and routes

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,17 @@
+class CommentsController < ApplicationController
+    def create 
+        @article = Article.find(params[:article_id])
+        @comment = @article.comments.create(comment_params)
+        redirect_to article_path(@article)
+    end
+    def destroy
+        @article = Article.find(params[:article_id])
+        @comment = Article.comments.find(params[:id])
+        @comment.destroy
+        redirect_to article_path(@article)
+    end
+    private 
+    def comment_params
+        params.require(:comment).permit(:name, :body)
+    end
+end
